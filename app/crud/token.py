c40efb6bf8579cbe53018efb5655aaf1e8c19d88
nbd <nir.bd@ametos.com>
2024-10-23 11:53:25 +0300

    improve():improve token crud file

--- app/crud/token.py
+++ app/crud/token.py
@@ -1,4 +1,3 @@
-from typing import Optional
 from sqlalchemy.orm import Session
 from app.crud.base import CRUDBase
 from app.models.token import TokenBlacklist
@@ -6,11 +5,8 @@ from app.schemas.token import TokenBlacklistCreate, TokenBlacklistRead
 
 
 class CRUDToken(CRUDBase[TokenBlacklist, TokenBlacklistCreate, TokenBlacklistRead]):
-    def get_by_token(self, db: Session, token: str) -> Optional[TokenBlacklist]:
-        return db.query(self.model).filter(self.model.token == token).first()
-
     def is_token_blacklisted(self, db: Session, token: str) -> bool:
-        return bool(self.get_by_token(db, token))
+        return bool(self.get_by_filter(db, filter_params={"token": token}))
 
 
 token = CRUDToken(TokenBlacklist)
